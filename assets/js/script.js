a12d2158d03a96adce7d78b05324ee0cc871d91b
Jean-marc Broutin <jmb@MacBook-Pro-de-Jean-marc.local>
2019-02-20 17:01:14 +0100

    cartes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
-function displayName(){
-    var name= prompt("Veuillez entrer votre nom");
-    document.getElementById("bienvenue").innerHTML="Bienvenue " + name + " !";
+// Affichage du nom personnalisé
 
+function getName() {
+    var n, q = 'Enter votre nom';
+    if (!window.sessionStorage) {  // if sessionStorage not supported
+        return window.prompt(q); // perform other action
+    }
+    n = window.sessionStorage.getItem('name');
+    
+    if (!n) {
+        n = window.prompt(q);
+        window.sessionStorage.setItem('name', n); 
+    }
+    document.getElementById("bienvenue").innerHTML = "Bienvenue " + n + " chez Shack Burger";
+    return n;
 }
