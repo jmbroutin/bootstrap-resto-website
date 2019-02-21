78f3d6d082d32361d3a32ec08d3353aaf8aea450
Jean-marc Broutin <jmb@MacBook-Pro-de-Jean-marc.local>
2019-02-21 11:28:47 +0100

    openclose

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,75 @@ function getName() {
     
     if (!n) {
         n = window.prompt(q);
+        if (n==="" || n===null){
+            n="inconnu";}
         window.sessionStorage.setItem('name', n); 
     }
-    document.getElementById("bienvenue").innerHTML = "Bienvenue " + n + " chez Shack Burger";
+    document.getElementById("bienvenue").innerHTML = "Bienvenue " + n + " !!!";
     return n;
 }
+// Affichage de la date et de l'heure
+(function dateTime() {
+    nomsDesJours = new Array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi");
+    nomsDesMois = new Array("janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre");
+    var d = new Date();
+    var jour = d.getDay();
+    var mois = d.getMonth();
+    var annee = d.getFullYear();
+    var date = d.getDate();
+    var heure = pad2(d.getHours());
+    var minutes = pad2(d.getMinutes());
+
+    document.getElementById("dateTime").innerHTML = "Nous sommes le " + nomsDesJours[jour] + " " + date + " " + nomsDesMois[mois] + " " + annee + ". Il est " + heure + "h" + minutes +".";
+    setTimeout(dateTime,60000);
+})();
+
+/*setInterval(function dateTime() {
+    nomsDesJours = new Array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi");
+    nomsDesMois = new Array("janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre");
+    var d = new Date();
+    var jour = d.getDay();
+    var mois = d.getMonth();
+    var annee = d.getFullYear();
+    var date = d.getDate();
+    var heure = pad2(d.getHours());
+    var minutes = pad2(d.getMinutes());
+
+    document.getElementById("dateTime").innerHTML = "Nous sommes le " + nomsDesJours[jour] + " " + date + " " + nomsDesMois[mois] + " " + annee + ". Il est " + heure + "h" + minutes +".";
+},60000);
+*/
+function pad2(number) {
+    return (number < 10 ? '0' : '') + number;
+};
+
+// fonction vérifiant si les restaurants sont ouverts ou fermés
+(function openClose(){
+    var d = new Date();
+    var jour = d.getDay();
+    var heure = d.getHours();
+    var open="";
+
+    if (jour==0){
+        open="Nos restaurants sont actuellement fermés.";
+    }
+    else if (4 < jour && jour <= 6){
+        if (10 < heure && heure < 23){
+            open="Nos restaurants sont actuellement ouverts.";
+        }
+        else{
+            open="Désolé, nos restaurants sont actuellement fermés.";
+        }
+    }
+    else if (1 <= jour && jour < 5){
+        if (10 < heure && heure < 22){
+            open="Nos restaurants sont actuellement ouverts.";
+        }
+        else{
+            open="Désolé, nos restaurants sont actuellement fermés.";
+        }
+    }
+document.getElementById("openClose").innerHTML= open;
+setTimeout(openClose,60000);
+})();
+
+
